749d6cdfdd1dc30bf692816e9f7e793e8b35f53f
Yash Gulati <yashgulatii007@gmail.com>
2023-06-23 19:16:09 +0530

    Pausing & Playing Music with Spotify API

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -13,6 +13,22 @@ export default class MusicPlayer extends Component {
     super(props);
   }
 
+  pauseSong() {
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/spotify/pause", requestOptions);
+  }
+
+  playSong() {
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/spotify/play", requestOptions);
+  }
+
   render() {
     const songProgress = (this.props.time / this.props.duration) * 100;
 
@@ -30,7 +46,11 @@ export default class MusicPlayer extends Component {
               {this.props.artist}
             </Typography>
             <div>
-              <IconButton>
+              <IconButton
+                onClick={() => {
+                  this.props.is_playing ? this.pauseSong() : this.playSong();
+                }}
+              >
                 {this.props.is_playing ? <Pause /> : <PlayArrow />}
               </IconButton>
               <IconButton>
